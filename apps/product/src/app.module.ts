0e9de5ded81e8067d201388c558f929cd1c4ad85
Blanc <topblade6@naver.com>
2025-05-07 14:26:23 +0900

    feat(product): Product 마이크로서비스 세팅 및 Docker 환경 구성
    - product 마이크로서비스 앱 신규 생성 및 모듈 구성
    - Dockerfile 및 docker-compose 설정으로 컨테이너 환경 구성
    - TypeORM + ConfigModule 세팅

---
+++ apps/product/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from 'apps/product/src/product/product.module';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_URL: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.getOrThrow('DB_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ProductModule,
+  ],
+})
+export class AppModule {}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,7 @@ export default tseslint.config(
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-floating-promises': 'off',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
